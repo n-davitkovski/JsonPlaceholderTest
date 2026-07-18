e7068894fb01099ab8fb5c82d5bdd337ac414923
Nikola Davitkovski <davitkovskinikola16@gmail.com>
2026-07-18 16:12:18 +0200

    Add Lombok annotations to JsonPlaceModelResponseGET

--- src/main/java/models/response/JsonPlaceModelResponseGET.java
+++ src/main/java/models/response/JsonPlaceModelResponseGET.java
@@ -1,5 +1,14 @@
 package models.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class JsonPlaceModelResponseGET {
     int userId, id;
     String title, body;
